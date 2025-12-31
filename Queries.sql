c16f38fd9b14e01de843828054b671148abeb9af
DilipMG <92212194+DilipMG@users.noreply.github.com>
2025-12-31 19:49:35 +0530

    Add files via upload

--- Queries.sql
+++ Queries.sql
@@ -245,6 +245,212 @@ select employee_id,
 count(*) over(partition by team_id) as team_size
 from employee_assignment order by employee_id ;
 
+-- table: Countries
+create table countries
+(country_id int,
+ country_name varchar(25),
+ constraint pk primary key (country_id)
+);
 
+insert into Countries VALUES
+(2,'USA'),(3,'Australia'),(7,'Peru'),(5,'China'),(8,'Morocco'),(9,'Spain');
 
+select * from countries;
 
+-- Table: Weather
+create table if not exists Weather
+(
+country_id int,
+weather_state int,
+day date,
+constraint pk PRIMARY KEY (country_id, day)
+);
+
+insert into Weather VALUES
+(2,15,'2019-11-01'),(2,12,'2019-10-28'),(2,12,'2019-10-27'),(3,-2,'2019-11-10'),
+(3,0,'2019-11-11'),(3,3,'2019-11-12'),(5,16,'2019-11-07'),(5,18,'2019-11-09'),
+(5,21,'2019-11-23'),(7,25,'2019-11-28'),(7,22,'2019-12-01'),(7,20,'2019-12-02'),
+(8,25,'2019-11-05'),(8,27,'2019-11-15'),(8,31,'2019-11-25'),(9,7,'2019-10-23'),(9,3,'2019-12-23');
+
+select * from Weather;
+
+-- Write an SQL query to find the type of weather in each country for November 2019.
+-- The type of weather is:
+-- 1. Cold if the average weather_state is less than or equal 15,
+-- 2. Hot if the average weather_state is greater than or equal to 25, and
+-- 3. Warm otherwise.
+
+select c.country_name, 
+	   case
+         when avg(w.weather_state) <=15 then 'Cold'
+         when avg(w.weather_state) >= 25 then 'Hot'
+         else 'Warm'
+       end as weather_type
+from weather w inner join countries c
+on c.country_id = w.country_id
+where day between '2019-11-01' and '2019-11-30'
+group by c.country_id
+order by weather_type;
+
+-- Write an SQL query to find the average selling price for each product. 
+-- average_price should be rounded to 2 decimal places.
+
+Create table prices
+(
+product_id int,
+start_date date,
+end_date date,
+price int,
+constraint pk primary key(product_id, start_date, end_date)
+);
+
+create table unitssold
+(
+product_id int,
+purchase_date date,
+units int
+);
+
+insert into Prices VALUES
+(1,'2019-02-17','2019-02-28',5),(1,'2019-03-01','2019-03-22',20),
+(2,'2019-02-01','2019-02-20',15),(2,'2019-02-21','2019-03-31',30);
+
+insert into unitssold VALUES
+(1,'2019-02-25',100),(1,'2019-03-01',15),(2,'2019-02-10',200),(2,'2019-03-22',30);
+
+SELECT a.product_id
+, round(SUM(a.units * b.price) / SUM(a.units), 2) AS
+average_price
+FROM UnitsSold a
+JOIN Prices b
+ON (a.product_id = b.product_id)
+where (a.purchase_date >= b.start_date
+AND a.purchase_date <= b.end_date)
+GROUP BY product_id;
+
+-- Write an SQL query to report the first login date for each player.
+create table activity
+(
+player_id int,
+device_id int,
+event_date date,
+games_played int,
+constraint pk primary key(player_id, event_date)
+);
+
+INSERT into Activity values
+(1,2,'2016-03-01',5),(1,2,'2016-05-02',6),(2,3,'2017-06-25',1),
+(3,1,'2016-03-02',0),(3,4,'2018-07-03',5);
+
+select player_id, min(event_date) as first_login
+from activity
+group by player_id
+order by player_id;
+
+select
+tmp.player_id,tmp.event_date as first_login
+from (select *,
+row_number() over(partition by player_id ) as row_num
+from Activity) tmp
+where tmp.row_num = 1;
+
+-- This query assigns the row number based on the event_date ascneding order. 
+SELECT *,
+	ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY event_date) AS row_num
+FROM activity;
+
+-- select the 3rd highest salary taker. 
+create table salary
+(
+emp_name varchar(20),
+salary int
+);
+
+insert into salary values ('Dilip',20000),('Aman',10000),('Ravi',25000),('Satish',99000),('Kiran',1000),
+						  ('Pritvi',98000),('Raki',31000),('Suraj',99000),('Rajesh',87000),('Nagesh',35000);   
+                          
+with ranked as	
+  (select *, 
+    dense_rank() over (order by salary desc) as salary_sorted
+  from salary)
+  select emp_name, salary from ranked where salary_sorted = 3;
+ 
+select  emp_name, salary
+from
+(select *, dense_rank() over(order by salary desc) as salary_sorted from salary) as tmp
+where tmp.salary_sorted = 3;
+     
+select * from salary order by salary desc limit 1 offset 2;   -- offset = SKIP
+     
+-- Write an SQL query to report the device that is first logged in for each player.     
+select player_id, device_id
+from (select *,
+       row_number() over (partition by player_id order by device_id) as first_device
+from activity) as tmp
+where tmp.first_device = 1;
+
+-- Write an SQL query to get the names of products that have at least 100 units ordered in February 2020 and their amount.
+create table if not exists Products
+(
+product_id int,
+product_name VARCHAR(50),
+product_category VARCHAR(50),
+constraint pk PRIMARY KEY (product_id)
+);
+insert into Products values (1,'Leetcode Solutions','Book'),(2,'Jewels of Stringology','Book'),
+                            (3,'HP','Laptop'),(4,'Lenovo','Laptop'),(5,'Leetcode Kit','T-shirt');
+
+-- Table: Orders
+create table if not exists Orders
+(
+product_id int,
+order_date date,
+unit int,
+constraint fk1 FOREIGN KEY (product_id) REFERENCES
+Products(product_id)
+);
+insert into Orders values
+(1,'2020-02-05',60),(1,'2020-02-10',70),(2,'2020-01-18',30),(2,'2020-02-11',80),
+(3,'2020-02-17',2),(3,'2020-02-24',3),(4,'2020-03-01',20),(4,'2020-03-04',30),
+(4,'2020-03-04',60),(5,'2020-02-25',50),(5,'2020-02-27',50),(5,'2020-03-01',50);
+
+select * from Orders;
+select * from Products;
+
+-- Write an SQL query to get the names of products that have at least 100 units ordered in February 2020 and their amount.
+
+select p.product_id, product_name, product_category, tmp.total_units_sold
+from products p inner join 
+  (select product_id, sum(unit) as total_units_sold
+    from orders o
+    where  
+      o.order_date >= '2020-02-01' and
+      o.order_date <= '2020-02-29'
+    group by o.product_id) as tmp
+on tmp.product_id = p.product_id;
+
+-- Write an SQL query to find the users who have valid emails.
+-- A valid e-mail has a prefix name and a domain where:
+-- The prefix name is a string that may contain letters (upper or lower case), digits, underscore 
+-- '_', period '.', and/or dash '-'. The prefix name must start with a letter. The domain ● is '@leetcode.com'.    
+
+-- Table: Users
+create table if not exists Users
+(
+user_id int,
+name varchar(50),
+mail varchar(50),
+constraint pk PRIMARY KEY (user_id)
+);
+insert into Users VALUES
+(1,'Winston','winston@leetcode.com'),(2,'Jonathan','jonathanisgreat'),
+(3,'Annabelle','bella-@leetcode.com'),(4,'Sally','sally.come@leetcode.com'),
+(5,'Marwan','quarz#2020@leetcode.com'),(6,'David','david69@gmail.com'),
+(7,'Shapiro','.shapo@leetcode.com');
+select * from Users;
+       
+SELECT *
+FROM Users
+WHERE REGEXP_LIKE(mail, '^[a-zA-Z][a-zA-Z0-9\_\.\-]*@leetcode.com');       
+
+    
